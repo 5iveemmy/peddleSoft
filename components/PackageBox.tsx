437d80bceb21158d8402d69b1942fccbd43d5014
5iveemmy <ainemmanuel87@gmail.com>
2024-02-06 17:10:20 +0100

    feat: create package box container

---
+++ components/PackageBox.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {SvgXml} from 'react-native-svg';
+
+const arrow = `<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g opacity="0.4">
+<path d="M8.91003 20.42L15.43 13.9C16.2 13.13 16.2 11.87 15.43 11.1L8.91003 4.58" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</svg>
+`;
+
+interface Props {
+  title: string;
+  description: string;
+  icon: string;
+  bgColor: string;
+  borderColor: string;
+}
+
+const PackageBox = ({
+  title,
+  description,
+  icon,
+  borderColor,
+  bgColor,
+}: Props) => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: bgColor, borderColor: borderColor},
+      ]}>
+      <View style={styles.subContainer}>
+        <SvgXml xml={icon} />
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>{title}</Text>
+          <Text style={styles.subText}>{description}</Text>
+        </View>
+      </View>
+      <SvgXml xml={arrow} />
+    </View>
+  );
+};
+
+export default PackageBox;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    width: '100%',
+    height: 105,
+    justifyContent: 'space-between',
+    borderRadius: 12,
+    padding: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  subContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  textContainer: {
+    width: 200,
+  },
+  header: {
+    fontWeight: '600',
+    fontSize: 20,
+  },
+  subText: {
+    paddingTop: 3,
+  },
+});
--- screens/Packages.tsx
+++ screens/Packages.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import PackageBox from '../components/PackageBox';
+import {packageData} from './data';
 
 const Packages = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <Text style={styles.text}>Packages</Text>
+      <View style={styles.packagesContainer}>
+        {packageData.map(({icon, borderColor, bgColor, title, description}) => (
+          <PackageBox
+            borderColor={borderColor}
+            bgColor={bgColor}
+            icon={icon}
+            title={title}
+            description={description}
+          />
+        ))}
+      </View>
     </SafeAreaView>
   );
 };
@@ -16,6 +29,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'GeneralSans-Bold',
     fontWeight: '700',
+  },
+  packagesContainer: {
+    paddingTop: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
   },
   safeArea: {
     padding: 24,
