d0a43cce45aec03713313c3fcca4cc8b037d0160
5iveemmy <ainemmanuel87@gmail.com>
2024-02-07 09:21:36 +0100

    feat: add health insurance page

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 
 import Packages from './screens/Packages';
+import HealthInsurance from './screens/HealthInsurance';
 
 const Stack = createStackNavigator();
 
@@ -22,6 +23,7 @@ const App = (): React.JSX.Element => {
         }}
         initialRouteName="Packages">
         <Stack.Screen name="Packages" component={Packages} />
+        <Stack.Screen name="HealthInsurance" component={HealthInsurance} />
       </Stack.Navigator>
     </NavigationContainer>
   );
